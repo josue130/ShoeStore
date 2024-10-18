7dd358b4709e2f89c622f3b27de0a261f3ed6a94
josue130 <josuelopez67i90@gmail.com>
2024-10-18 15:47:16 -0600

    feat(product): Add product controller

---
+++ Store.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Store.Services.ProductAPI.Models;
+using Store.Services.ProductAPI.Models.Dto;
+using Store.Services.ProductAPI.Service.IService;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Store.Services.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly ResponseDto _response;
+        public ProductAPIController(IProductService productService)
+        {
+            _productService = productService;
+            _response = new();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> data = await _productService.GetAllProducts();
+                _response.Result = data;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            try
+            {
+                ProductDto data = await _productService.GetProductById(id);
+                _response.Result = data;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+        [HttpPost]
+        //[Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> Post([FromBody] ProductDto model)
+        {
+            try
+            {
+                await _productService.AddProduct(model);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+        [HttpPut]
+        //[Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> Put([FromBody] ProductDto model)
+        {
+            try
+            {
+                await _productService.UpdateProduct(model);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        //[Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await _productService.DeleteProduct(id);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+    }
+}
--- Store.Services.ProductAPI/Program.cs
+++ Store.Services.ProductAPI/Program.cs
@@ -2,6 +2,8 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Store.Services.ProductAPI;
 using Store.Services.ProductAPI.Data;
+using Store.Services.ProductAPI.Service;
+using Store.Services.ProductAPI.Service.IService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 });
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
+builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
