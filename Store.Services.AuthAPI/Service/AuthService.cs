af22de8fed98904b06f553cda0387d74d28711e5
josue130 <josuelopez67i90@gmail.com>
2024-10-18 14:20:23 -0600

    Add authentication service for Auth API

---
+++ Store.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Store.Services.AuthAPI.Data;
+using Store.Services.AuthAPI.Models;
+using Store.Services.AuthAPI.Models.Dto;
+using Store.Services.AuthAPI.Service.IService;
+
+namespace Store.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AuthService(AppDbContext db, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public async Task<bool> AssignRole(string email, string roleName)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(user => user.Email.ToLower() == email.ToLower());
+            if (user != null)
+            {
+                if (!_roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+                {
+                    //create role if it does not exist
+                    _roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+                }
+                await _userManager.AddToRoleAsync(user, roleName);
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(user => user.UserName.ToLower() == loginRequestDto.UserName.ToLower());
+
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+            if (user == null || !isValid)
+            {
+                return new LoginResponseDto() { User = null, Token = "" };
+            }
+
+            UserDto userDTO = new()
+            {
+                Email = user.Email,
+                ID = user.Id,
+                FullName = user.FullName,
+                PhoneNumber = user.PhoneNumber
+            };
+            var roles = await _userManager.GetRolesAsync(user);
+            var token = "";
+            LoginResponseDto loginResponseDto = new LoginResponseDto()
+            {
+                User = userDTO,
+                Token = token
+            };
+
+            return loginResponseDto;
+        }
+
+        public async Task<string> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            ApplicationUser user = new()
+            {
+                UserName = registrationRequestDto.Email,
+                Email = registrationRequestDto.Email,
+                NormalizedEmail = registrationRequestDto.Email.ToUpper(),
+                FullName = registrationRequestDto.FullName,
+                PhoneNumber = registrationRequestDto.PhoneNumber
+            };
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+                if (result.Succeeded)
+                {
+                    var userToReturn = _db.ApplicationUsers.First(user => user.Email == registrationRequestDto.Email);
+
+                    UserDto userDto = new()
+                    {
+                        FullName = userToReturn.FullName,
+                        ID = userToReturn.Id,
+                        Email = userToReturn.Email,
+                        PhoneNumber = userToReturn.PhoneNumber
+                    };
+                    return "";
+                }
+                else
+                {
+                    return result.Errors.FirstOrDefault().Description;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return "Error Encountered";
+        }
+    }
+}
