c7d8c5d19faf1a0d77c20c588858520ddc1831cc
josue130 <josuelopez67i90@gmail.com>
2024-10-18 18:05:14 -0600

    feat(ShoppingCart): Add Cart table with initial migration

---
+++ Store.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Services.ShoppingCartAPI.Models;
+using System.Collections.Generic;
+
+namespace Store.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+
+    }
+}
---
+++ Store.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        [Key]
+        public Guid CartDetailsId { get; set; }
+        public Guid CartHeaderId { get; set; }
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+        public Guid ProductId { get; set; }
+        public int Count { get; set; }
+
+    }
+}
