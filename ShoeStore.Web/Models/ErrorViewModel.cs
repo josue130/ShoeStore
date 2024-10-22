5d86131883c3eea1d0793eb86655fb510b454e4b
josue130 <josuelopez67i90@gmail.com>
2024-10-22 14:27:18 -0600

    feat(Web): Add asp.net web

---
+++ ShoeStore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoeStore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
