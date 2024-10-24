21f998043557baac3977c75cdbc01d9555d1597f
josue130 <josuelopez67i90@gmail.com>
2024-10-24 17:58:05 -0600

    feat(web): redesign card datails

--- ShoeStore.Web/wwwroot/js/site.js
+++ ShoeStore.Web/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function zoom(event) {
+    const img = event.currentTarget.querySelector('.card-img-top');
+    const { left, top, width, height } = event.currentTarget.getBoundingClientRect();
+    const x = ((event.clientX - left) / width) * 100;
+    const y = ((event.clientY - top) / height) * 100;
+
+    img.style.transform = `scale(2)`;
+    img.style.transformOrigin = `${x}% ${y}%`;
+}
+
+function resetZoom() {
+    const img = document.querySelector('.card-img-top');
+    img.style.transform = `scale(1)`;
+}
