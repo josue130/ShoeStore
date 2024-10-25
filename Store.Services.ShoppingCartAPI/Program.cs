9fe6d987043dda63c84822f9da8a78ecfb0d2a87
josue130 <josuelopez67i90@gmail.com>
2024-10-25 17:56:22 -0600

    Feat add ApplyMigration method to handle pending migrations

--- Store.Services.ShoppingCartAPI/Program.cs
+++ Store.Services.ShoppingCartAPI/Program.cs
@@ -35,6 +35,21 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+ApplyMigration();
+
 app.MapControllers();
 
 app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
